4ecdbfce6013ce04a2b722c2a9dbe7440e9cd4b2
paulmorio <schererpaul@rocketmail.com>
2017-10-22 23:58:53 +0100

    Numpy Array Basics

--- basics/numpyArray_basics.py
+++ basics/numpyArray_basics.py
@@ -29,3 +29,72 @@ x.reshape((1, 3))
 # row vector via newaxis
 x[np.newaxis, :]
 
+# Array concatenation
+## One dimesnsion
+x = np.array([1, 2, 3])
+y = np.array([3, 2, 1])
+np.concatenate([x, y]) # makes one longer array
+
+z = [99, 99, 99]
+print(np.concatenate([x, y, z])) # concatenate more than two arrays at once
+
+# two dimension
+grid = np.array([[1, 2, 3],
+                 [4, 5, 6]])
+
+# concatenate along the first axis
+np.concatenate([grid, grid])
+
+# $output
+# array([[1, 2, 3],
+#        [4, 5, 6],
+#        [1, 2, 3],
+#        [4, 5, 6]])
+
+# concatenate along the second axis (zero-indexed)
+np.concatenate([grid, grid], axis=1)
+# $output
+# array([[1, 2, 3, 1, 2, 3],
+#        [4, 5, 6, 4, 5, 6]])
+
+# it might be eaiser to understand np.vstack and np.hstack functions
+x = np.array([1, 2, 3])
+grid = np.array([[9, 8, 7],
+                 [6, 5, 4]])
+
+# vertically stack the arrays
+np.vstack([x, grid])
+# ## output
+# array([[1, 2, 3],
+#        [9, 8, 7],
+#        [6, 5, 4]])
+
+# horizontally stack the arrays
+y = np.array([[99],
+              [99]])
+np.hstack([grid, y])
+
+# # output
+# array([[ 9,  8,  7, 99],
+#        [ 6,  5,  4, 99]])
+
+# np.dstack will stack arrays along the third axis, there are obviously more possible dimensions
+# but it really starts to make little sense at this point.
+
+# Splitting
+x = [1, 2, 3, 99, 99, 3, 2, 1]
+x1, x2, x3 = np.split(x, [3, 5])
+print(x1, x2, x3)
+
+grid = np.arange(16).reshape((4, 4))
+grid
+
+# vertical split
+upper, lower = np.vsplit(grid, [2])
+print(upper)
+print(lower)
+
+# horizontal split
+left, right = np.hsplit(grid, [2])
+print(left)
+print(right)
